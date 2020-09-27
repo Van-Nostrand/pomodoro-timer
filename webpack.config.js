9d6c8382c9c1f8419b557b433a1d6f9768bf69df
Van-Nostrand <dmdoull43@gmail.com>
2020-09-26 19:12:57 -0700

    quick commit before pull

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,13 @@
 let path = require('path');
 let HtmlWebpackPlugin = require('html-webpack-plugin');
+let MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
   entry: './index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'index_bundle.js'
+    filename: 'bundle.js',
+    publicPath: ""
   },
   module: {
     rules: [
@@ -15,8 +17,29 @@ module.exports = {
         exclude: /node_modules/
       },
       {
+        test: /\.s[ac]ss$/i,
+        use: [
+          'style-loader',
+          'css-loader',
+          'resolve-url-loader',
+          {
+            loader: 'sass-loader',
+            options: {
+              implementation: require('node-sass'),
+              sassOptions: {
+                file: "src/sass/main.scss",
+                outFile: "public/style.css"
+              }
+            }
+          }
+        ]
+      },
+      {
         test: /\.css$/, 
-        use: ['style-loader', 'css-loader'], 
+        use: [
+          MiniCssExtractPlugin.loader,
+          "css-loader"
+        ],
         exclude: /node_modules/
       },
       {
